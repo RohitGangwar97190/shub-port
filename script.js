1a20b24944e1e1f50cf10336c6c6e081d290531f
Rohit gangwar <rohitgangwar971912@gmail.com>
2025-05-20 02:57:56 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,21 @@
+var typed = new Typed(".auto_name", {
+    strings: ["Shubh Aggarwal", "Flutter Developer", "Programmer", "Backend Developer"],
+    typeSpeed: 105,
+    backSpeed: 10,
+    loop: true
+});
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname, event) {
+    for (let tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (let tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link"); // Fix: "link" not "links"
+    document.getElementById(tabname).classList.add("active-tab"); // Fix: match class
+}
+ 
